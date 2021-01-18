080bad6ad875fe459d9e660dcf2b2b09b8389353
Raja Kalavala <rajakalavala55@gmail.com>
2021-01-18 23:09:28 +0530

    Complete Post UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import { useState } from "react";
 import "./App.css";
+import Post from "./components/Post/Post";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "raja",
+      caption: "This is caption",
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbkG8Ut6pR95qddRmWKkqVeOa3iuwv8-BzTg&usqp=CAU",
+    },
+    {
+      username: "k.raja",
+      caption: "This is caption",
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI-1qjrRaNrrF_62w7IaKO9P-WYvQASraDfg&usqp=CAU",
+    },
+  ]);
   return (
     <div className="app">
       <div className="app__header">
@@ -10,6 +26,14 @@ function App() {
           alt=""
         />
       </div>
+
+      {posts.map((post) => (
+        <Post
+          username={post.username}
+          caption={post.caption}
+          imageUrl={post.imageUrl}
+        />
+      ))}
     </div>
   );
 }
