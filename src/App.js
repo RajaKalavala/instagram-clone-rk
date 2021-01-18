141079b06a3833c2caa9e6b9b82eb905fdd51556
Raja Kalavala <rajakalavala55@gmail.com>
2021-01-18 20:07:43 +0530

    Add logo and setup initial header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
-      <h1>Hello</h1>
+      <div className="app__header">
+        <img
+          className="app__headerImages"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
+      </div>
     </div>
   );
 }
